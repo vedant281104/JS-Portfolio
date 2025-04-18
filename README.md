e6a775e3e6d6a9a35bc656968ef8e2b67343f319
Vedant <vu2715@srmist.edu.in>
2025-04-18 07:55:56 +0530

    updated portfolio

---
+++ README.md
@@ -0,0 +1,5 @@
+## Portfolio-Website
+Portfolio website build using HTML5, CSS3, JavaScript and jQuery.
+
+<a href="https://luminous-gelato-4ac180.netlify.app/" target="_blank">**Visit Now** 🚀</a>
+
---
+++ projects/script.js
@@ -0,0 +1,143 @@
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Projects | Portfolio Vedant Upadhyay";
+            $("#favicon").attr("href", "/assets/images/favicon.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "/assets/images/favhand.png");
+        }
+    });
+
+
+// fetch projects start
+function getProjects() {
+    return fetch("projects.json")
+        .then(response => response.json())
+        .then(data => {
+            return data
+        });
+}
+
+
+function showProjects(projects) {
+    let projectsContainer = document.querySelector(".work .box-container");
+    let projectsHTML = "";
+    
+    projects.forEach(project => {
+        // Construct the image path
+        let imgSrc = `../assets/images/projects/${project.image}.png`;
+
+        console.log("Trying to load image:", imgSrc);  // Add this line
+
+        // Check if the image exists (we will use a fallback if it doesn't)
+        let image = new Image();
+        image.src = imgSrc;
+        image.onload = function() {
+            console.log(`Image loaded successfully: ${imgSrc}`);
+        };
+        image.onerror = function() {
+            console.log(`Image failed to load: ${imgSrc}`);
+            imgSrc = 'assets/images/projects/default.png'; // Use a default image in case of error
+        };
+
+        // Create project HTML with the dynamic image
+        projectsHTML += `
+        <div class="grid-item ${project.category}">
+            <div class="box tilt" style="width: 380px; margin: 1rem">
+                <!-- Image with dynamic source -->
+                <img draggable="false" src="${imgSrc}" alt="${project.name}" />
+                <div class="content">
+                    <div class="tag">
+                        <h3>${project.name}</h3>
+                    </div>
+                    <div class="desc">
+                        <p>${project.desc}</p>
+                        <div class="btns">
+                            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                            <a href="${project.links.rep}" class="btn" target="_blank">Reports <i class="fas fa-file-alt"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    });
+    
+    projectsContainer.innerHTML = projectsHTML;
+
+    // Isotope for filtering projects
+    var $grid = $('.box-container').isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        masonry: {
+            columnWidth: 200
+        }
+    });
+
+    // Filter projects on button click
+    $('.button-group').on('click', 'button', function () {
+        $('.button-group').find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+}
+
+
+
+
+getProjects().then(data => {
+    showProjects(data);
+})
+// fetch projects end
+
+// Start of Tawk.to Live Chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+// End of Tawk.to Live Chat
+
+// disable developer mode
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
